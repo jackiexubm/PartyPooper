712be5a30c1388645bb02fe36d1deef1e5c383d9
Jackie Xu <jackiexubm1@gmail.com>
2017-01-10 16:06:20 -0500

    Add article image and author/news outlet label

--- PartyPooper/MentionCell.swift
+++ PartyPooper/MentionCell.swift
@@ -38,26 +38,57 @@ class MentionCell: UICollectionViewCell{
         return view
     }()
     
+    let mentionPicture: UIImageView = {
+        let view: UIImageView = UIImageView()
+        view.backgroundColor = UIColor.green
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.image = #imageLiteral(resourceName: "article_1_photo")
+        view.contentMode = .scaleAspectFill
+        view.layer.masksToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let authorAndNewsOutletLabel: UILabel = {
+        let view: UILabel = UILabel()
+        view.text = "RALPH NADER • THE WASHINGTON POST"
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.textAlignment = NSTextAlignment.right
+        view.textColor = UIColor.gray
+        view.font = UIFont(name: "HelveticaNeue-Medium", size: 8)
+        return view
+    }()
+    
     func setupViews(){
+        let pictureHeight = 9 / 16 * UIScreen.main.bounds.width
         
         addSubview(topCellSeperator)
         addSubview(bottomCellSeperator)
         addSubview(openMentionButton)
+        addSubview(mentionPicture)
+        addSubview(authorAndNewsOutletLabel)
+        
         addConstraintWithString("H:|[v0]|")
         addConstraintWithString("V:|[v0(1)]")
         addConstraintWithString("H:|[v1]|")
         addConstraintWithString("V:[v1(1)]|")
         addConstraintWithString("H:|[v2]|")
         addConstraintWithString("V:|[v2]-1-|")
+        addConstraintWithString("H:|[v3]|")
+        addConstraintWithString("V:|[v3(\(pictureHeight))]-7-[v4(8)]")
+        addConstraintWithString("H:|[v4]-7-|")
+        
         
         
     }
     
     func addConstraintWithString(_ str: String){
         let views: [String:UIView] = [
-        "v0":topCellSeperator,
-        "v1":bottomCellSeperator,
-        "v2":openMentionButton
+            "v0":topCellSeperator,
+            "v1":bottomCellSeperator,
+            "v2":openMentionButton,
+            "v3":mentionPicture,
+            "v4":authorAndNewsOutletLabel
         ]
         
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: str, options: NSLayoutFormatOptions(), metrics: nil, views: views))
--- PartyPooper/MentionsViewController.swift
+++ PartyPooper/MentionsViewController.swift
@@ -38,7 +38,7 @@ class MentionsViewController: UICollectionViewController, UICollectionViewDelega
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: 250)
+        return CGSize(width: collectionView.frame.width, height: 450)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
