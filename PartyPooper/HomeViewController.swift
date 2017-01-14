a01a21abcc2bd5cd9165cf5d7459e901ccbf2c86
Jackie Xu <jackiexubm1@gmail.com>
2017-01-14 18:08:54 -0500

    Add button and prompt to recommended intro view

--- PartyPooper/HomeViewController.swift
+++ PartyPooper/HomeViewController.swift
@@ -12,10 +12,11 @@ class HomeViewController: UIViewController {
     
     var didFillOutStances: Bool = false
     
-    let segmentedControl: UISegmentedControl = {
+    lazy var segmentedControl: UISegmentedControl = {
         let view: UISegmentedControl = UISegmentedControl(items: ["All","Recommended"])
+        view.selectedSegmentIndex = 0
         view.layer.borderWidth = 1
-        //view.layer.borderColor = UIColor.gray.cgColor
+        view.addTarget(self, action: #selector(segmentedControlSwitch), for: .valueChanged)
         view.layer.cornerRadius = 3
         return view
     }()
@@ -25,14 +26,6 @@ class HomeViewController: UIViewController {
         view = HomeView()
         navigationItem.titleView = segmentedControl
         
-        let recIntroVC = RecommendedIntroViewController()
-        addChildViewController(recIntroVC)
-        recIntroVC.didMove(toParentViewController: self)
-        recIntroVC.view.translatesAutoresizingMaskIntoConstraints = false
-        (view as! HomeView).containerView.addSubview(recIntroVC.view)
-        (view as! HomeView).containerView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":recIntroVC.view]))
-        (view as! HomeView).containerView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":recIntroVC.view]))
-        
         let allViewCV = AllViewController()
         addChildViewController(allViewCV)
         allViewCV.didMove(toParentViewController: self)
@@ -41,12 +34,38 @@ class HomeViewController: UIViewController {
         (view as! HomeView).containerView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":allViewCV.view]))
         (view as! HomeView).containerView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":allViewCV.view]))
         
+        let recIntroVC = RecommendedIntroViewController()
+        addChildViewController(recIntroVC)
+        recIntroVC.didMove(toParentViewController: self)
+        recIntroVC.view.translatesAutoresizingMaskIntoConstraints = false
+        (view as! HomeView).containerView.addSubview(recIntroVC.view)
+        (view as! HomeView).containerView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":recIntroVC.view]))
+        (view as! HomeView).containerView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":recIntroVC.view]))
+        
+        childViewControllers[1].view.isHidden = true
         
         
         super.viewDidLoad()
     }
     
+    func segmentedControlSwitch(){
+        if segmentedControl.selectedSegmentIndex == 0{
+            childViewControllers[0].view.isHidden = false
+            childViewControllers[1].view.isHidden = true
+        }else{
+            childViewControllers[0].view.isHidden = true
+            if didFillOutStances {
+                
+            }else{
+                childViewControllers[1].view.isHidden = false
+            }
+        }
+    }
     
+    func openSurvey(){
+        print("open survey")
+    
+    }
     
     
     //    override func viewWillAppear(_ animated: Bool) {
