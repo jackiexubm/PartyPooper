1869e0822f41965fa288b0efd50efa1870bff039
Byungzoon Kang <gyungsu7@gmail.com>
2017-01-14 15:32:19 -0500

    Create DonateView

---
+++ PartyPooper/DonateView.swift
@@ -0,0 +1,64 @@
+//
+//  DonateView.swift
+//  PartyPooper
+//
+//  Created by David Kang on 1/14/17.
+//  Copyright © 2017 Debug Politics Team. All rights reserved.
+//
+
+import UIKit
+
+
+
+class DonateView: UIView {
+    
+    
+    override init(frame: CGRect){
+    super.init(frame: frame)
+    backgroundColor = UIColor.white
+    setupViews()
+    }
+    
+    let donateLabel: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor.red
+        return button
+    }()
+    
+    let imageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.backgroundColor = UIColor.blue
+        return image
+    }()
+    
+    
+    
+    func setupViews() {
+        addSubview(imageView)
+        imageView.addSubview(donateLabel)
+        
+        
+        addConstraintsWithString("H:|-100-[v0(20)]")
+        addConstraintsWithString("V:|-300-[v0(20)]")
+        
+        addConstraintsWithString("H:|[v1]|")
+        addConstraintsWithString("V:|-64-[v1]-|")
+
+
+    }
+    
+    
+
+    func addConstraintsWithString(_ str:String){
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: str, options: NSLayoutFormatOptions(), metrics: nil, views:
+            ["v0":donateLabel,
+             "v1":imageView
+            ]))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
