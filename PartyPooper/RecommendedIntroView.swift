01aabaa41b43c3deb6c0b915908062d291601030
Jackie Xu <jackiexubm1@gmail.com>
2017-01-14 21:49:25 -0500

    Complete issue picker view. Buttons switch states correctly

--- PartyPooper/RecommendedIntroView.swift
+++ PartyPooper/RecommendedIntroView.swift
@@ -22,7 +22,7 @@ class RecommendedIntroView: UIView{
         view.isScrollEnabled = false
         view.translatesAutoresizingMaskIntoConstraints = false
         view.textAlignment = NSTextAlignment.center
-        view.text = "It looks like you haven't filled out your stances yet!"
+        view.text = "It looks like you haven't chosen issues you care about yet!"
         view.font = UIFont(name: "HelveticaNeue", size: 25)
         return view
     }()
@@ -33,7 +33,7 @@ class RecommendedIntroView: UIView{
         view.layer.borderWidth = 1
         view.layer.borderColor = UIColor.lightGray.cgColor
         view.layer.cornerRadius = 3
-        view.setTitle("Fill Stances", for: .normal)
+        view.setTitle("Fill Out Form", for: .normal)
         view.titleLabel?.font = UIFont(name: "HelveticaNeue-Medium", size: 30)
         view.setTitleColor(UIColor.black, for: .normal)
         view.addTarget(self, action: #selector(HomeViewController.openSurvey), for: .touchUpInside)
@@ -46,8 +46,8 @@ class RecommendedIntroView: UIView{
         addSubview(fillStancesButton)
         
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-30-[v0]-30-|", options: NSLayoutFormatOptions(), metrics: nil, views: views))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-30-[v0(100)]-10-[v1(60)]", options: NSLayoutFormatOptions(), metrics: nil, views: views))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[v1(200)]", options: NSLayoutFormatOptions(), metrics: nil, views: views))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-140-[v0(110)]-10-[v1(60)]", options: NSLayoutFormatOptions(), metrics: nil, views: views))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[v1(220)]", options: NSLayoutFormatOptions(), metrics: nil, views: views))
         addConstraint(NSLayoutConstraint(item: fillStancesButton, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0))
 
         
---
+++ PartyPooper/StancesFormViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StancesFormViewController.swift
+//  PartyPooper
+//
+//  Created by Jackie Xu on 1/14/17.
+//  Copyright © 2017 Debug Politics Team. All rights reserved.
+//
+
+import UIKit
+class StancesFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        view = IssuesChooserView()
+    }
+    
+}
+
